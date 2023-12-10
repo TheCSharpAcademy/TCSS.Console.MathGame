2da113fbce1199d09e7d70c890ff8e140933da46
Pablo <thecsharpacademy@gmail.com>
2023-12-10 12:35:04 +1000

    Menu with string verbatim

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿Console.WriteLine("Hello! This is your math's game. It's great that you're working on improving yourself.");
-Console.ReadLine();
+﻿Console.WriteLine("Hello! This is your math's game. It's great that you're working on improving yourself.\n");
+Console.WriteLine("Press any key to continue.");
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine(@"What game would you like to play?
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+V - View Previous Games");
+
