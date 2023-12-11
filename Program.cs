dd49461c1fe331b322e41a81cd224b1d545394bf
Pablo <thecsharpacademy@gmail.com>
2023-12-11 12:21:45 +1000

    Quit option on menu

--- Program.cs
+++ Program.cs
@@ -16,8 +16,37 @@ A - Addition
 S - Subtraction
 M - Multiplication
 D - Division
-V - View Previous Games");
+V - View Previous Games
+Q - Quit The Program");
 
 userOption = Console.ReadKey().KeyChar;
-Console.WriteLine($"\nYou chose the option: {userOption}");
+
+if (char.ToLower(userOption) == 'a')
+{
+    Console.WriteLine("Addition Selected");
+}
+else if (char.ToLower(userOption) == 's')
+{
+    Console.WriteLine("Subtraction selected");
+}
+else if (char.ToLower(userOption) == 'm')
+{
+    Console.WriteLine("Multiplication selected");
+}
+else if (char.ToLower(userOption) == 'd')
+{
+    Console.WriteLine("Division selected");
+}
+else if (char.ToLower(userOption) == 'v')
+{
+    Console.WriteLine("View previous games selected");
+}
+else if (char.ToLower(userOption) == 'q')
+{
+    Console.WriteLine("Goodbye");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
 
