6c25340482cd983e16762306ae72b9e738a18c16
Pablo <thecsharpacademy@gmail.com>
2023-12-11 11:25:49 +1000

    small changes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
-﻿Console.WriteLine("Hello! This is your math's game. It's great that you're working on improving yourself.\n");
+﻿Console.WriteLine($"What's your name?");
+string name = Console.ReadLine();
+DateTime date = DateTime.Now;
+int gamesPlayed = 0;
+char userOption;
+decimal averageScore;
+bool isScoreEmpty = true;
+
+Console.WriteLine($"Hello {name}! Today is {date}. This is your math's game. It's great that you're working on improving yourself.\n");
 Console.WriteLine("Press any key to continue.");
 Console.ReadKey();
 Console.Clear();
+Console.WriteLine($"Games played: {gamesPlayed}");
 Console.WriteLine(@"What game would you like to play?
 A - Addition
 S - Subtraction
 M - Multiplication
 D - Division
 V - View Previous Games");
+
+userOption = Console.ReadKey().KeyChar;
+Console.WriteLine($"\nYou chose the option: {userOption}");
 
